b7f20ddb2cab6a197a116991e26832861f4c1d9c
itsumrat <itsumrat@gmail.com>
2016-11-25 00:30:33 +0600

    expertness added

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -2,6 +2,20 @@ jQuery(document).ready(function($){
 
 	var navigation = $('#nav-main').okayNav();
 
-	
+
+  $("#bluecircle").percircle({
+  	progressBarColor: "#30bae7",
+  	secs: 15,
+  	timeUpText: 'Finally!'
+  });
+  $("#yellowcircle").percircle({
+  	progressBarColor: "#d74680"
+  });
+  $("#redcircle").percircle({
+  	progressBarColor: "#15c7a8"
+  });
+  $("#blackcircle").percircle({
+  	progressBarColor: "#eb7d4b"
+  });
 
 });

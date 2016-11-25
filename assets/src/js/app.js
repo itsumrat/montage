4a8fce742ede4239b8286379926d4ad33b08f44f
itsumrat <itsumrat@gmail.com>
2016-11-26 02:53:36 +0600

    step to responsive

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($){
 
 	var navigation = $('#nav-main').okayNav();
 
-
+	// Circle
 	$("#bluecircle").percircle({
 		progressBarColor: "#30bae7",
 		secs: 15,
@@ -18,15 +18,39 @@ jQuery(document).ready(function($){
 		progressBarColor: "#eb7d4b"
 	});
 
+	// credit where credit's due; http://thecodeplayer.com/walkthrough/ripple-click-effect-google-material-design
+	var element, circle, d, x, y;
+	$(".btn").click(function(e){
+	  
+		element = $(this);
+	  
+		if(element.find(".circle").length == 0)
+			element.prepend("<span class='circle'></span>");
+			
+		circle = element.find(".circle");
+		circle.removeClass("animate");
+		
+		if(!circle.height() && !circle.width())
+	  {
+			d = Math.max(element.outerWidth(), element.outerHeight());
+			circle.css({height: d, width: d});
+		}
+		
+		x = e.pageX - element.offset().left - circle.width()/2;
+		y = e.pageY - element.offset().top - circle.height()/2;
+		
+		circle.css({top: y+'px', left: x+'px'}).addClass("animate");
+	})
+
 });
 
 
 //Change Navbar color when scroll
-/*$(window).on("scroll", function() {
-    if($(window).scrollTop() > 50) {
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 150) {
         $("header").addClass("active");
     } else {
         //remove the background property so it comes transparent again (defined in your css)
        $("header").removeClass("active");
     }
-});*/
+});
